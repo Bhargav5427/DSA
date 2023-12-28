1b4c08281bebcb5dce03aa518f58c7556715740c
Bhargav <128204030+Bhargav5427@users.noreply.github.com>
2023-12-28 08:20:03 +0530

    Update array-insert-2.js

--- array-insert-2.js
+++ array-insert-2.js
@@ -1,14 +1,14 @@
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let position = 9;
 let element = 25;
-let array_2 = 0;
+let temp = 0;
 
 for (let i = arr.length; i >= position; i--) {
     if (i == position) {
-        array_2 = arr[i];
+        temp = arr[i];
         arr[i] = element;
     }
 }
-arr[arr.length] = array_2;
+arr[arr.length] = temp;
 
 console.log(arr);
