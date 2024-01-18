5578cbe35bbebbf141cf8b2111690e7f7458d9c4
Bhargav <128204030+Bhargav5427@users.noreply.github.com>
2024-01-18 08:28:15 +0530

    Update binary-search.js

--- binary-search.js
+++ binary-search.js
@@ -17,8 +17,9 @@ function binarySearch(arr, l, r, x) {
 
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let x = 10 ;
+let l = 0 ;
 let n = arr.length - 1;
-let result = binarySearch(arr, 0, n, x);
+let result = binarySearch(arr, l, n, x);
 if (result == -1) {
     console.log("Element is not present in array ");
 }
